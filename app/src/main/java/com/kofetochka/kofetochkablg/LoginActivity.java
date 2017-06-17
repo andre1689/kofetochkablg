00af7844effae9033b19ff285e5d1cbf91e42185
Andrew Smola <andrew.smol.g@gmail.com>
2017-06-17 20:46:50 +0300

    Create function Identification

--- app/src/main/java/com/kofetochka/kofetochkablg/LoginActivity.java
+++ app/src/main/java/com/kofetochka/kofetochkablg/LoginActivity.java
@@ -3,10 +3,83 @@ package com.kofetochka.kofetochkablg;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.kofetochka.inquiry.InquiryIdentification;
+import com.kofetochka.inquiry.InquiryRole;
 
 public class LoginActivity extends Activity {
+
+    private EditText etLogin, etPassword;
+
+    private String Login = null;
+    private String Password = null;
+    private String Surname = null;
+    private String Name = null;
+    private String Middlename = null;
+    private String Block = null;
+    private String Role = null;
+    private String NameRole = null;private
+
+    InquiryIdentification inquiryIdentification;
+    InquiryRole inquiryRole;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_layout);
+
+        etLogin = (EditText)findViewById(R.id.editText_Login);
+        etPassword = (EditText)findViewById(R.id.editText_Password);
+    }
+
+    public void ViewDB (View view){
+        Login = etLogin.getText().toString();
+        inquiryIdentification = new InquiryIdentification();
+        inquiryIdentification.start(Login);
+
+        try {
+            inquiryIdentification.join();
+        } catch (InterruptedException e) {
+            Log.e("Pass 0", e.getMessage());
+        }
+
+        Password = inquiryIdentification.resPassword();
+        Surname = inquiryIdentification.resSurname();
+        Name = inquiryIdentification.resName();
+        Middlename = inquiryIdentification.resMiddlename();
+        Block = inquiryIdentification.resBlock();
+        Role = inquiryIdentification.resRole();
+
+        inquiryRole = new InquiryRole();
+        inquiryRole.start(Role);
+
+        try {
+            inquiryRole.join();
+        } catch (InterruptedException e) {
+            Log.e("Pass 0", e.getMessage());
+        }
+
+        NameRole = inquiryRole.resNameRole();
+
+        if (Login!=null){
+            if (etPassword.getText().toString().equals(Password)){
+                if(Block.equals("0")){
+                    //Intent intent = new Intent(this,WorkActivity.class);
+                    //intent.putExtra("Login", Login);
+                    //intent.putExtra("NameRole",NameRole);
+                    //startActivity(intent);
+                    Toast.makeText(this, "Верный пароль.", Toast.LENGTH_LONG).show();
+                }
+                else Toast.makeText(this, "Ваша учетная запись заблокирована. Обратитесь к администратору.", Toast.LENGTH_LONG).show();
+            }
+            else Toast.makeText(this, "Пара логин/пароль введены неверно", Toast.LENGTH_LONG).show();
+        }
+        else Toast.makeText(this, "Пара логин/пароль введены неверно", Toast.LENGTH_LONG).show();
+
     }
 }
