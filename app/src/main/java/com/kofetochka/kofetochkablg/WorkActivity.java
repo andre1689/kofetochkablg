aaa424b44b6161b7a7cc0bf08b748f9a29312cf3
Andrew Smola <andrew.smol.g@gmail.com>
2017-06-18 12:46:19 +0300

    Create NavigationDrawer

--- app/src/main/java/com/kofetochka/kofetochkablg/WorkActivity.java
+++ app/src/main/java/com/kofetochka/kofetochkablg/WorkActivity.java
@@ -1,12 +1,23 @@
 package com.kofetochka.kofetochkablg;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
-public class WorkActivity extends ActionBarActivity {
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+
+public class WorkActivity extends AppCompatActivity {
+
+    private Drawer result = null;
+    private AccountHeader accountHeaderResult = null;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -16,7 +27,37 @@ public class WorkActivity extends ActionBarActivity {
         Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
         if (toolbar != null){
             setSupportActionBar (toolbar);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
+
+        initializeNavigationDrawer(toolbar);
+        result.getHeader();
+
+    }
+
+    private void initializeNavigationDrawer(Toolbar toolbar) {
+
+        accountHeaderResult = new AccountHeaderBuilder()
+                .withActivity(this)
+                .withHeaderBackground(R.color.colorPrimaryDark)
+                .addProfiles(
+                        new ProfileDrawerItem().withName("Mike Penz").withEmail("mikepenz@gmail.com")
+                )
+                .build();
+
+        result = new DrawerBuilder(this)
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .withAccountHeader(accountHeaderResult)
+                .withDisplayBelowStatusBar(false)
+                .withActionBarDrawerToggleAnimated(true)
+                .addDrawerItems(
+                        new PrimaryDrawerItem()
+                                .withName(R.string.item_home),
+                        new DividerDrawerItem()
+                )
+                .build();
+
     }
 
 }
